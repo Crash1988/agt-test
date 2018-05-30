7f2478c581f56a0a12b2289042095718626cd1e9
Gonzalez Alayon <dgonzalezalayon@GAIG.COM>
2018-05-30 10:36:33 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+import { RestComponent, TooltipComponent  } from './rest/rest.component';
+import { CustomRowComponent } from './custom-row/custom-row.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { GenericTableModule } from '@angular-generic-table/core';
+import { ColumnSettingsModule } from '@angular-generic-table/column-settings';
+import { DragulaModule } from 'ng2-dragula';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestComponent,
+    CustomRowComponent,
+    TooltipComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    GenericTableModule,
+    ColumnSettingsModule,
+    DragulaModule,
+    NgbModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    CustomRowComponent,
+    
+		TooltipComponent,
+		]
 })
 export class AppModule { }
